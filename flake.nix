c2a1758063dadcec1a874f71d15d96bf5351c640
SonOfMithras <ammar.alriyamy@gmail.com>
2026-02-01 00:21:28 -0600

    fix(nix): use qt6Packages.qtstyleplugin-kvantum

--- flake.nix
+++ flake.nix
@@ -43,14 +43,14 @@
               pkgs.kdePackages.kxmlgui
               pkgs.kdePackages.kwindowsystem
               # Kvantum for Qt6
-              pkgs.qt6Packages.kvantum
+              pkgs.qt6Packages.qtstyleplugin-kvantum
             ];
 
             # Ensure lookandfeeltool and kvantummanager are in the PATH of the wrapper
             qtWrapperArgs = [
               "--prefix PATH : ${pkgs.lib.makeBinPath [ 
                   pkgs.kdePackages.plasma-workspace 
-                  pkgs.qt6Packages.kvantum 
+                  pkgs.qt6Packages.qtstyleplugin-kvantum 
               ]}"
             ];
 
